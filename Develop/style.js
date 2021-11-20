751eb82862396951c14d87065cfd46e0654e2639
David Gavino <ggavino2@uccs.edu>
2021-11-19 19:32:35 -0700

    init/ Time blocks added

---
+++ Develop/style.js
@@ -0,0 +1,22 @@
+// const colorcode = document.getElementsByClassName("color");
+// let currentHour = parseInt(moment().format('H'));
+
+// Array.from(colorcode).forEach(colorcode => {
+//     let
+//         colorcodeIdString = colorcode.id,
+//         colorcodeHour;
+//     if (colorcodeIdString) {
+//         colorcodeHour = parseInt(colorcodeIdString);
+//     }
+//     if (colorcodeHour) {
+//         if (currentHour === colorcodeHour) {
+//             setColor(green);
+//         } else if ((currentHour < colorcodeHour) && (currentHour > colorcodeHour - 6)) {
+//             setColor(color, "blue");
+//         } else if ((currentHour > colorcodeHour) && (currentHour < colorcodeHour + 6)) {
+//             setColor(color, "red");
+//         } else {
+//             setColor(color, "white");
+//         }
+//     }
+// });
